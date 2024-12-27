43a56f0247ec48e9a7acb9c239dfad798e2b4c60
RiteshKumarNayak <riteshnayakgudu49@gmail.com>
2024-12-27 13:25:22 +0530

    updated required version

--- 16-Azure-IAC-DevOps/terraform-manifests/versions.tf
+++ 16-Azure-IAC-DevOps/terraform-manifests/versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~>1.5.6" # Minor version upgrades are allowed
+  required_version = ">=1.5.6" # Minor version upgrades are allowed
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
